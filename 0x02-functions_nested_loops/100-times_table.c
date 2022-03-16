e67e40849b8c31d4ded93d77980ec8f7eba1b9bf
Julius Odunuga <40062040+Julisam@users.noreply.github.com>
2022-03-16 13:46:53 +0100

    Update 100-times_table.c

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -9,35 +9,22 @@
 
 void print_times_table(int n)
 {
-	int x, y;
-
-	if (n <= 15 && n >= 0)
-	{
-	for (y = 0; y <= n; y++)
-	{
-		for (x = 0; x <= n; x++)
-		{
-			int product = x * y;
-			int []digits;
-
-			while (product)
-			{
-				int i = sizeof(digits) / sizeof(digits[0]);
-				products[i + 1] = product / 10;
-				product /= 10;
-			}
-			for
-			(
-			int i = (sizeof(digits) / sizeof(digits[0]));
-			i >= 0;
-			i--
-			)
-			{
-				putchar('0' + digits[i]);
-				putchar(',');	
-			}
-		}
-		_putchar('\n');
-	}
-	}
+    if ((n <= 15) && (n >= 0))
+    {
+        int row = 0, col = 0, product;
+        
+        for (row = 0; row <=n; row++)
+    	{
+            for (col =0; col<=n; col++)
+            {
+                product  = row*col;
+                printf("%d", product);
+                if (col == n)
+                    break;
+                printf(",  ");
+            }
+            _putchar('\n');
+        }
+    }
 }
+	
