8dc1f66573d06f06e55883cfa696a0976e60401a
Julius Odunuga <odunugajulius1@gmail.com>
2022-03-17 05:50:56 -0700

    task2

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _isupper - check if character is uppercase
+ * @c : integer value
+ * Return: 1 if c is lower case 0 otherwise
+ *
+ */
+
+int _isdigit(int c)
+{
+	if (c >= 48 && c <= 57)
+	{
+		return (1);
+	}
+	else
+		return (0);
+}
