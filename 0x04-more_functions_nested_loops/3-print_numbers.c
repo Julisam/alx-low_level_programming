99182a1413f9a5fa5e240fe602ba0eb967a7b0c6
Julius Odunuga <odunugajulius1@gmail.com>
2022-03-17 06:39:42 -0700

    task3

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_numbers - print number 0-9
+ * Return : 1 if c is lower case 0 otherwise
+ *
+ */
+
+void print_numbers(void)
+{
+	int c;
+
+	for (c = 48; c <= 57; c++)
+	{
+		_putchar(c);
+	}
+	_putchar('\n');
+
+}
