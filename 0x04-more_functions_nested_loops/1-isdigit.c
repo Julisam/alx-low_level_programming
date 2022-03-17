716aab1679ef13e3a95669ed2f1494c4345fbe2a
Julius Odunuga <odunugajulius1@gmail.com>
2022-03-17 06:00:28 -0700

    betty style

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * _isupper - check if character is uppercase
+ * _isdigit - check if character is uppercase
  * @c : integer value
  * Return: 1 if c is lower case 0 otherwise
  *
