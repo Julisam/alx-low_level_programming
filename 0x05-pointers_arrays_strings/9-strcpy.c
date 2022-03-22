6fd9bb24c1c3f9773f773797dceac33d7692e2cf
Julius Odunuga <odunugajulius1@gmail.com>
2022-03-22 10:53:07 +0100

    task 9

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ *_strcpy - copies the string pointed to by src
+ *@dest: pointer to destination
+ *@src: pointer to source
+ *
+ *Return: void.
+ */
+char *_strcpy(char *dest, char *src)
+{
+	int i;
+
+	for (i = 0; i < n; i++)
+	{
+		printf("%d", a[i]);
+		if (i != n - 1)
+		{
+			printf(",");
+			printf(" ");
+		}
+	}
+	printf("\n");
+}
