844966235aecfa35937720c3cc09e14f2fb2c2af
Julius Odunuga <odunugajulius1@gmail.com>
2022-03-23 14:52:32 +0100

    task2

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ *_strncpy - copies a string
+ *@dest: destination strnig
+ *@src:  source string
+ *@n:  no of bytes to use
+ *Return: destination pointer
+ */
+
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i;
+
+	for (i = 0; i < n && src[i] != '\0'; i++)
+	{
+		dest[i] = src[i];
+	}
+
+	for (; i < n; i++)
+		dest[i] = '\0';
+
+	return (dest);
+}
