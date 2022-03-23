f9706c60dcc72d6531a83c1737b90a2b02e0dd68
Julius Odunuga <odunugajulius1@gmail.com>
2022-03-23 13:57:05 +0100

    betty style corrected2

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,22 +1,21 @@
 #include "main.h"
 /**
- *_strlen - finds and outputs length of string.
- *@s: pointer to string.
- *
- *Return: length of a string.
- */
-
-/**
  *_strcat - concatenates two strings
  *@dest: destination string.
  *@src:  source string
  *Return: pointer to destination string.
  */
 
+/**
+ *_strlen - finds and outputs length of string.
+ *@s: pointer to string.
+ *Return: length of a string.
+ */
+
 char *_strcat(char *dest, char *src)
 {
 	int last = _strlen(dest);
-	
+
 	while (*src != '\0')
 	{
 		*(dest + last) = *src;
