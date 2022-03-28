d2c7de12cf5b1d145add3c26570b272ba8be5ec7
Julius Odunuga <40062040+Julisam@users.noreply.github.com>
2022-03-28 22:01:23 +0100

    Update 5-strstr.c

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -1,4 +1,5 @@
-#include "main.h" 
+#include "main.h"
+
 /**
  * _strncmp - compares two strings within b bytes.
  * @s1: string to check
@@ -10,6 +11,7 @@
 int _strncmp(char *s1, char *s2, unsigned int b)
 {
 	unsigned int i;
+	
 	for (i = 0; i <= b && *s1 == *s2 && *s1 != '\0' && *s2 != '\0'; i++)
 	{
 		s1++;
@@ -26,6 +28,7 @@ int _strncmp(char *s1, char *s2, unsigned int b)
 char *_strstr(char *haystack, char *needle)
 {
 	unsigned int len;
+	
 	/*Get length of needle for strncmp*/
 	len = 0;
 	while (needle[len] != '\0')
